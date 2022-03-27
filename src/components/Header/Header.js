9f21e36dccf8c6b8e5a341d36687d4e347b67cc6
zaforemon20 <abuzaforemon@gmail.com>
2022-03-28 01:01:11 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1 className='site-title'>Car Chooser</h1>
+      <h3>Select 4 Car</h3>
+    </div>
+  );
+};
+
+export default Header;

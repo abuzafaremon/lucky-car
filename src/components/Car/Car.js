cca9eb03441eeff6e45c61760b48a3d29f10da28
zaforemon20 <abuzaforemon@gmail.com>
2022-03-28 21:48:08 +0600

    cart updated with click event handler

--- src/components/Car/Car.js
+++ src/components/Car/Car.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Car.css'
 
-const Car = ({car}) => {
-  const {name, price, thumb}=car
+const Car = ({car, handleAddToCart,}) => {
+  const {name, price, thumb}=car;
+
   return (
     <div className='car'>
       <img src={thumb} alt="" />
-      <h3>Name: {name} </h3>
-      <h4>Price: ${price} </h4>
+      <div className="car-info">
+        <h3>Name: {name} </h3>
+        <p>Price: {price} </p>
+      </div>
+        <button onClick={()=> handleAddToCart(car)} className='cart-btn'>
+          <p>Add to Cart</p>
+        </button>
     </div>
   );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,23 +4,34 @@ import './Shop.css'
 
 const Shop = () => {
   const [cars, setCars] = useState([]);
+  const [cart, setCart] = useState([]);
+
   useEffect( ()=>{
     fetch('cars.json')
     .then(res => res.json())
     .then(data => setCars(data))
   },[])
 
+  const handleAddToCart = (car) =>{
+    console.log(car);
+    const newCart = [...cart, car];
+    setCart(newCart);
+  }
+
   return (
     <div className='shop-container'>
       <div className="cars-container">
         {
-          cars.map(car => <Car key={car.id}
+          cars.map(car => <Car 
+          key={car.id}
           car={car}
+          handleAddToCart={handleAddToCart}
           ></Car>)
         }
       </div>
       <div className="cart-container">
         <h2>This is cart</h2>
+        <p>Selected Items: {cart.length}</p>
       </div>
     </div>
   );

8b0eafdd522d1be9995827518a1eb8429e3de1f9
zaforemon20 <abuzaforemon@gmail.com>
2022-03-30 14:51:33 +0600

    4 itme added and random item show done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash} from '@fortawesome/free-solid-svg-icons';
 import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = ({car}) => {
+  const {name, thumb, price,id}=car;
+  
   return (
-    <div className="cart-info">
-      <h2>This is cart</h2>
-      <p>Selected Items: {cart.length}</p>
-    </div>
+        <div className="selected-car">
+          <img src={thumb} alt="" />
+          <div>
+          <h4>{name}</h4>
+          <h6>Price: {price}</h6>
+          </div>
+          <button><FontAwesomeIcon className='icon' icon={faTrash}></FontAwesomeIcon></button>
+        </div>
   );
 };
 
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck} from '@fortawesome/free-solid-svg-icons';
 import Car from '../Car/Car';
 import Cart from '../Cart/Cart';
 import './Shop.css'
@@ -6,6 +8,8 @@ import './Shop.css'
 const Shop = () => {
   const [cars, setCars] = useState([]);
   const [cart, setCart] = useState([]);
+  const[randomCar, setRandomCar] = useState({});
+  // console.log(randomCar)
 
   useEffect( ()=>{
     fetch('cars.json')
@@ -14,10 +18,23 @@ const Shop = () => {
   },[])
 
   const handleAddToCart = (car) =>{
-    console.log(car);
-    const newCart = [...cart, car];
-    setCart(newCart);
+      console.log(cart.length)
+      // console.log(newCart)
+      if(cart.length <= 3 && cart.indexOf(car)=== -1){
+        const newCart = [...cart, car];
+        
+        // const addedCar = newCart.map(car => car.id);
+        
+        setCart(newCart);
+      }
   }
+  const chooseOne = () =>{
+   const randomCarNum = Math.floor(Math.random()* cart.length);
+   const randomCar = (cart[randomCarNum]);
+   setRandomCar(randomCar);
+  }
+
+  
 
   return (
     <div className='shop-container'>
@@ -31,7 +48,31 @@ const Shop = () => {
         }
       </div>
       <div className="cart-container">
-        <Cart cart = {cart}></Cart>
+        <div className="cart-info">
+          <h2 className='cart-title'>Selected Cars</h2>
+          <p>Selected Items: {cart.length}</p>
+        {
+          cart.map(car => <Cart 
+            key={car.id} 
+            car={car}
+            ></Cart>)
+        }
+        {Object.keys(randomCar).length>0 && (<div className='random-car'>
+          <img src={randomCar.thumb} alt="" />
+          <div>
+          <h4>{randomCar.name}</h4>
+          <h6>Price: {randomCar.price}</h6>
+          </div>
+          <FontAwesomeIcon className='icon' icon={faCheck}></FontAwesomeIcon>
+        </div>)}
+        {Object.keys(cart).length>3 && (<div className="btn-group">
+          <button onClick={chooseOne} className='choose-btn'>Choose 1 for me</button>
+          <br />
+          <button className='choose-again-btn'>
+            <p>Choose Again</p>
+          </button>
+        </div>)}
+        </div>
       </div>
     </div>
   );
